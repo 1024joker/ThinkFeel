0b817a41cab3b172166f2dd04ba18ad3731e1833
joker <hi1024@aiyun.com>
2018-08-24 17:12:21 +0800

    最新pay

--- src/java/com/mxthd/controller/GoodsClassController.java
+++ src/java/com/mxthd/controller/GoodsClassController.java
@@ -1,5 +1,8 @@
 package com.mxthd.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.mxthd.bean.Goods;
 import com.mxthd.service.GoodsClassService;
 import com.mxthd.service.GoodsInfoService;
 import org.apache.ibatis.annotations.Param;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class GoodsClassController {
     @Autowired
@@ -19,13 +24,18 @@ public class GoodsClassController {
     @RequestMapping("/gift")
     public ModelAndView showGoodsClass(@RequestParam(value = "t",required = false) Integer t,
                                        @RequestParam(value = "min",required = false) Integer min,
-                                       @RequestParam(value = "max",required = false) Integer max){
+                                       @RequestParam(value = "max",required = false) Integer max,
+                                       @RequestParam(value = "p",defaultValue = "1") Integer p){
         ModelAndView modelAndView = new ModelAndView("gift");
         modelAndView.addObject("goodsClass",goodsClassService.getAll());
-        modelAndView.addObject("goods",goodsInfoService.getHome(t,min,max));
+        PageHelper.startPage(p,6);
+        List<Goods> home = goodsInfoService.getHome(t, min, max);
+        PageInfo page = new PageInfo(home,5);//连续显示5页
+        modelAndView.addObject("goods",page);
         modelAndView.addObject("t",t);
         modelAndView.addObject("min",min);
         modelAndView.addObject("max",max);
+        modelAndView.addObject("count",goodsInfoService.getHome(t, min, max).size());
         return modelAndView;
     }
 }
