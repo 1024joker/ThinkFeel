4a824aa4a244ddc5abbb20bc9fba435cc871c8df
jylstart <2959822780@qq.com>
2018-08-28 20:11:52 +0800

    增加了模态框

--- src/java/com/mxthd/dao/PayMapper.java
+++ src/java/com/mxthd/dao/PayMapper.java
@@ -9,5 +9,6 @@ public interface PayMapper {
     public void payAdd(@Param("pay") Pay pay);
     public int payHuidiao(Integer id);
     public Pay findByPPID(String ppid);
+    //后台用户的id值来查询消费信息
     public List<Pay> findByUid(Integer uid);
 }
